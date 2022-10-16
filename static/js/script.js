fc0c3d2357e9ff84c9476423a29bdeed9d70ee06
Michelle <79888145+michelled01@users.noreply.github.com>
2022-10-16 15:33:30 -0500

    Add buttons

--- static/js/script.js
+++ static/js/script.js
@@ -9,10 +9,9 @@ document.addEventListener("DOMContentLoaded", loadDOM)
 
 //load dom function
 function loadDOM() {
-    // player.innerHTML = currentPlayer
+    player.innerHTML = currentPlayer
     // playAgain.addEventListener("click", reset)
     var cell = document.getElementsByClassName('cell');
-    // let squares = document.querySelectorAll(".board div")
     Array.from(cell).forEach(square => {
         square.addEventListener("click", selectCell)
     })
@@ -36,38 +35,32 @@ function selectCell(cell) {
         .catch(err => console.error(err));
 
         
-    // let squares = document.querySelectorAll(".board div")
-    // let click = parseInt(this.dataset.id)
-    // if (squares[click + 7].classList.contains("taken") && !squares[click].classList.contains("taken")) {
-    //     if (currentPlayer === 1) {
-    //         currentPlayer = 2
-    //         player.innerHTML = currentPlayer
-    //         this.className = "player-one taken"
-    //         checkWon()
-    //     } else if (currentPlayer === 2) {
-    //         currentPlayer = 1
-    //         player.innerHTML = currentPlayer
-    //         this.className = "player-two taken"
-    //         checkWon()
-    //     }
-    //     if (box === 42) {
-    //         setTimeout(() => alert("boxes filled"), 300)
-    //         setTimeout(() => restart.style.display = "flex", 500)
-    //     }
-    // } else {
-    //     alert("You cannot build on an empty space or on a space that has been built on")
-    // }
+    if (currentPlayer === 1) {
+        currentPlayer = 2
+        player.innerHTML = currentPlayer
+        this.className = "player-one taken"
+        checkWon()
+    } else if (currentPlayer === 2) {
+        currentPlayer = 1
+        player.innerHTML = currentPlayer
+        this.className = "player-two taken"
+        checkWon()
+    }
+    if (box === 42) {
+        setTimeout(() => alert("boxes filled"), 300)
+        setTimeout(() => restart.style.display = "flex", 500)
+    }
 }
 
 //the checkWon function
 function checkWon() {
-    let squares = document.querySelectorAll(".board div")
+    let cell = document.querySelectorAll(".board div")
     for (let y = 0; y < winningArray.length; y++) {
         let square = winningArray[y]
-        if (square.every(q => squares[q].classList.contains("player-one"))) {
+        if (square.every(q => cell[q].classList.contains("player-one"))) {
             setTimeout(() => alert("player one(red) wins "), 200)
             setTimeout(() => restart.style.display = "flex", 500)
-        } else if (square.every(q => squares[q].classList.contains("player-two"))) {
+        } else if (square.every(q => cell[q].classList.contains("player-two"))) {
             setTimeout(() => alert("player two(yellow) wins"), 200)
             setTimeout(() => restart.style.display = "flex", 500)
         }
