c865e2cf33a1d2534a1b3185113a6912da2dd6e9
Michelle <79888145+michelled01@users.noreply.github.com>
2022-10-16 18:05:07 -0500

    Fixed winning behavior

--- static/js/script.js
+++ static/js/script.js
@@ -3,14 +3,16 @@ let player = document.querySelector(".player")
 let playAgain = document.querySelector(".playAgain")
 let restart = document.querySelector(".restart")
 let box = 0
-let winningArray = [];
+let winningArray = [[0,1,2,3],[4,5,6,7],[8,9,10,11],[12,13,14,15],
+                    [0,4,8,12],[1,5,9,13],[2,6,10,14],[3,7,11,15],
+                    [0,5,10,15],[3,6,9,12]];
 let currentPlayer = 1
 document.addEventListener("DOMContentLoaded", loadDOM)
 
 //load dom function
 function loadDOM() {
     player.innerHTML = currentPlayer
-    // playAgain.addEventListener("click", reset)
+    playAgain.addEventListener("click", reset)
     // var cell = document.getElementsByClassName('cell');
     // Array.from(cell).forEach(square => {
     //     square.addEventListener("click", selectCell)
@@ -34,45 +36,44 @@ function selectCell(cell) {
     })
     .catch(err => console.error(err));
 
-        
+
     if (currentPlayer === 1) {
-        // alert("current player is 1")
         currentPlayer = 2
         player.innerHTML = currentPlayer
         var myid = "#cell" + cell;
         document.querySelector(myid).className = "player1";
-        console.log(this)
-        // checkWon()
+        checkWon()
     } else if (currentPlayer === 2) {
-        // alert("current player is 2")
         currentPlayer = 1
         player.innerHTML = currentPlayer
         var myid = "#cell" + cell;
         document.querySelector(myid).className = "player2";
-        // checkWon()
+        checkWon()
     }
-    if (box === 42) {
+    if (box === 16) {
         setTimeout(() => alert("boxes filled"), 300)
-        setTimeout(() => restart.style.display = "flex", 500)
+        reset()
     }
 }
 
 //the checkWon function
 function checkWon() {
-    let cell = document.querySelectorAll(".board div")
+    // let cell = document.querySelectorAll(".board div")
     for (let y = 0; y < winningArray.length; y++) {
         let square = winningArray[y]
-        if (square.every(q => cell[q].classList.contains("player-one"))) {
-            setTimeout(() => alert("player one(red) wins "), 200)
-            setTimeout(() => restart.style.display = "flex", 500)
-        } else if (square.every(q => cell[q].classList.contains("player-two"))) {
-            setTimeout(() => alert("player two(yellow) wins"), 200)
-            setTimeout(() => restart.style.display = "flex", 500)
+        if (square.every(q => document.querySelector("#cell" + q).classList.contains("player1"))) {
+            setTimeout(() => alert("player one (red) wins"), 200)
+            setTimeout(() =>  {reset()}, "1000")
+        } else if (square.every(q => document.querySelector("#cell" + q).classList.contains("player2"))) {
+            setTimeout(() => alert("player two (blue) wins"), 200)
+            setTimeout(() =>  {reset()}, "1000")
         }
     }
 }
 function reset() {
-    board.innerHTML = ""
+    for (let i = 0; i < 16; i++) {        
+        var myid = "#cell" + i;
+        document.querySelector(myid).className = "cell";
+    }
     loadDOM()
-    restart.style.display = "none"
 }
